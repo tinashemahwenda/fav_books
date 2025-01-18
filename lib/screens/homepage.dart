849b3f53bbe4be594ac27b25bfd664ec596dd396
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-18 16:48:43 +0200

    Added some logic to handle favorite books

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _favoriteBooks = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _favoriteBooks = prefs.getStringList('favorites') ?? [];
+    });
+  }
+
+  Future<void> _toggleFavorite(String bookId) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      if (_favoriteBooks.contains(bookId)) {
+        _favoriteBooks.remove(bookId);
+      } else {
+        _favoriteBooks.add(bookId);
+      }
+    });
+    await prefs.setStringList('favourites', _favoriteBooks);
+  }
+
+  bool _isFavorite(String bookId) {
+    return _favoriteBooks.contains(bookId);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Text('Books')],
-      ),
+      body: 
     );
   }
 }
