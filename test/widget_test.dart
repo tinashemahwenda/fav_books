0c3ea90106073eeec7321eaba7b8abfae20c64f2
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-18 16:36:16 +0200

    Updated tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fav_books/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BooksApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
